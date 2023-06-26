18a093c0842fd783354e517104a1bd92ecd723ff
martinharriott <martin.roseharriott@gmail.com>
2023-06-26 13:50:01 -0400

    trying to fix gh pages

--- src/index.js
+++ src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
-import { BrowserRouter} from "react-router-dom";
+import { BrowserRouter, RouterProvider} from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter>
+    <>
+    sdasdadasdad
+    <RouterProvider>
         <App />
-    </BrowserRouter>
+    </RouterProvider>
+    </>
 );
